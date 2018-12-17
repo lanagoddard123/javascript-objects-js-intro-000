98e81312b014c01a66384158ab0ffb5b7160f380
lanasgoddard <lanasgoddard@gmail.com>
2018-12-17 15:42:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,3 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist['Slowdive'] = 'Alison'
   playlist['My Bloody Valentine'] = 'Sometimes'
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.Slowdive
+}
